714f59f11609707f61716734e364a3ed49229316
BSMOHAN <52600697+BSMOHAN@users.noreply.github.com>
2020-07-04 19:08:38 +0530

    Update prog.py

--- prog.py
+++ prog.py
@@ -1,16 +1,22 @@
 import os
 #Write your code here
 
-
-
-
-
-
-
-
-
-
-
+s = os.getcwd()
+s1 = "New Folder"
+s2 = "New Folder2"
+path = os.path.join(s,s1)
+path2 = os.path.join(s,s2)
+os.mkdir(path)
+os.chdir(path)
+q = os.getcwd()
+os.chdir("..")
+w1 = os.getcwd()
+os.rename(s1,s2)
+os.chdir(path2)
+p = os.getcwd()
+os.chdir("..")
+os.rmdir(path2)
+w = os.getcwd()
 
 with open('.hidden.txt','w') as outfile:
      outfile.write(s)
